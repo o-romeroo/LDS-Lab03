2325fe944b8ed9e112faa76b9bf821c76867eac4
Lucas Randazzo <lucasrandazzo2@gmail.com>
2024-10-13 16:25:13 -0300

    feat: add spring project

---
+++ backend/src/main/java/com/pucmg/lab03/Lab03Application.java
@@ -0,0 +1,15 @@
+package com.pucmg.lab03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.pucmg.lab03")
+public class Lab03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab03Application.class, args);
+	}
+
+}
