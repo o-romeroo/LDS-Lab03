8c484c65f07ed71394829da586172f3c0a526106
Douglas Machado <douglasgmachado98@gmail.com>
2024-10-15 16:03:02 -0300

    feeat: add class main in tests

--- backend/src/test/java/com/pucmg/lab02/Lab02ApplicationTests.java
+++ backend/src/test/java/com/pucmg/lab02/Lab02ApplicationTests.java
@@ -3,11 +3,13 @@ package com.pucmg.lab02;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.pucmg.lab03.Lab03Application;
+
+@SpringBootTest(classes = Lab03Application.class)
 class Lab02ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
