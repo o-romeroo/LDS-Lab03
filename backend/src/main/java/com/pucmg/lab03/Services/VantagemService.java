ac2115b3f9d8dac08d7ba59f13b80f4c4c6af82c
Lucas Randazzo <lucasrandazzo2@gmail.com>
2024-10-15 22:43:27 -0300

    fix: create advantage now select the correct user who owns the advantage

--- backend/src/main/java/com/pucmg/lab03/Services/VantagemService.java
+++ backend/src/main/java/com/pucmg/lab03/Services/VantagemService.java
@@ -5,9 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pucmg.lab03.Models.Empresa;
+import com.pucmg.lab03.Models.InstituicaoEnsino;
+import com.pucmg.lab03.Models.Usuario;
 import com.pucmg.lab03.Models.Vantagem;
+import com.pucmg.lab03.Repositories.UsuarioRepository;
 import com.pucmg.lab03.Repositories.VantagemRepository;
 import com.pucmg.lab03.dto.VantagemRequestDTO;
+
+import jakarta.transaction.Transactional;
+
 import java.io.IOException;
 
 
@@ -17,6 +24,9 @@ public class VantagemService {
     @Autowired
     private VantagemRepository vantagemRepository;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public Vantagem buscarVantagem(Long id) {
         return vantagemRepository.findById(id).orElseThrow(() -> new RuntimeException("Vantagem não encontrada"));
     }
@@ -25,11 +35,19 @@ public class VantagemService {
         return vantagemRepository.findAll();
     }
 
+    @Transactional
     public Vantagem criarVantagem(VantagemRequestDTO vantagemDto) throws IOException {
         Vantagem vantagem = new Vantagem();
         vantagem.setDetalhes(vantagemDto.getDescricao());
         vantagem.setPreco(vantagemDto.getPreco());
         vantagem.setImagem(vantagemDto.getImagem().getBytes());
+        Usuario usuario = usuarioRepository.findById(vantagemDto.getUsuarioId())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+                if (usuario instanceof Empresa) {
+                    vantagem.setEmpresa((Empresa) usuario);
+                } else {
+                    vantagem.setInstituicaoEnsino((InstituicaoEnsino)usuario);
+                }
         return vantagemRepository.save(vantagem);
 
     }
